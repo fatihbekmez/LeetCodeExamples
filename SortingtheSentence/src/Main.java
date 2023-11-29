314e8b2d3ead01ac908c1f1a88dd916018912ba7
Fatih Bekmez <132763414+fatihbekmez@users.noreply.github.com>
2023-11-29 23:55:46 +0300

    Add files via upload

---
+++ SortingtheSentence/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       String a;
+
+       Scanner sc = new Scanner(System.in);
+
+       a = sc.nextLine();
+
+       int c = 0;
+
+       if(a.contains("(") & a.contains(")")){
+           c++;
+       }
+
+       
+
+
+    }
+
+}
